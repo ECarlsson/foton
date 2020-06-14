a2f5817123d4aaf6c09a29b8798b03191a1d45e5
Eric Carlsson <32720134+ECarlsson@users.noreply.github.com>
2020-06-15 00:15:50 +0200

    Listen only to localhost
    Preparation for move to devil 2.0, serving node behind reverse proxy

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 });
 
-app.listen(port, () => {
+app.listen(port, 'localhost', () => {
   console.log(`Express listening on port ${port}...`);
 });
